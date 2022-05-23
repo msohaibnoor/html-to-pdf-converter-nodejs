1a5a24ca4933a4671b55ee894a857542b8ca168a
unknown <sohaib.noor@aquila360.com>
2022-05-23 11:36:20 +0500

    removed CIC's

--- routes/htmlToPdf.js
+++ routes/htmlToPdf.js
@@ -2,7 +2,6 @@ import express from "express";
 const router = express.Router();
 import { htmlConversion } from "../controllers/htmlConversion.js";
 
-// router.route("/").put( updateOrderToDelivered);
 router.post("/", htmlConversion);
 
 export default router;
