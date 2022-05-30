7b22a9cfbd73099b52642b9112998fde5003d36f
unknown <sohaib.noor@aquila360.com>
2022-05-30 10:20:15 +0500

    heroku sut up

--- index.js
+++ index.js
@@ -7,11 +7,14 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors())
+app.use(cors());
 
 app.use(express.json());
 
 app.use("/api/htmlToPdf", htmlToPdf);
+app.get("/", (req, res) => {
+  res.send("Server is running...");
+});
 
 const PORT = process.env.PORT || 5000;
 
